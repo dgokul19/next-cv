c9352232925105f75d4ee4b11d8a5898c312290f
dgokul19 <dgokul19@gmail.com>
2023-05-05 21:52:30 +0800

    Updates

--- static/CwT2qenqq3suKgieaL8lw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/36f7344755bf472c.css","static/chunks/pages/index-7ec251b7cebbee81.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
